a2ab73e72d8ea9d8f9153f342af2b24947238c4e
T162880 <T162880@MMSURMAWUW8.mmi.mig.corp>
2017-12-11 17:23:19 -0500

    vertx future

---
+++ hello-vertx/src/main/java/hello/Hello.java
@@ -0,0 +1,56 @@
+package hello;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        new Hello().sayHello2("Mark", future -> {
+            System.out.println(future.result());
+        });
+    }
+
+    public void sayHello2(String name, Handler<AsyncResult<String>> handler) {
+        Future<String> future = Future.<String>future();
+        future.setHandler(handler);
+
+        Future<String> hello = Future.<String>future();
+        concatWithHello(name, hello);
+
+        hello.compose(v -> {
+            Future<String> exclamation = Future.<String>future();
+            concatWithExclamation(v, exclamation.completer());
+            return exclamation;
+        })
+        .compose(v -> {
+            concatWithISay(v, future.completer());
+        }, future);
+    }
+
+    public void sayHello(String name, Handler<AsyncResult<String>> handler) {       
+        concatWithHello(name, hello -> {
+            if (hello.succeeded()) {
+                concatWithExclamation(hello.result(), exclamation -> {
+                    if (exclamation.succeeded()) {
+                        concatWithISay(exclamation.result(), handler);
+                    }
+                });
+            }
+        });
+    }
+
+    public void concatWithHello(String name, Handler<AsyncResult<String>> handler) {
+        handler.handle(Future.<String>succeededFuture("Hello " + name));
+    }
+
+    public void concatWithExclamation(String hello, Handler<AsyncResult<String>> handler) {
+        handler.handle(Future.<String>succeededFuture(hello + "!"));
+    }
+
+    public void concatWithISay(String message, Handler<AsyncResult<String>> handler) {
+        handler.handle(Future.<String>succeededFuture("I say: \"" + message + "\""));
+    }
+
+}
