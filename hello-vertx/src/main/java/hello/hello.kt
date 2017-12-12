33829a05b30c86013215150978e9fcfbf7cd0b49
T162880 <T162880@MMSURMAWUW8.mmi.mig.corp>
2017-12-12 17:22:41 -0500

    kotlin vertx future

---
+++ hello-vertx/src/main/java/hello/hello.kt
@@ -0,0 +1,28 @@
+package hello
+
+import io.vertx.core.AsyncResult
+import io.vertx.core.Future
+import io.vertx.core.Handler
+
+fun main(args: Array<String>) {
+    sayHello("Mark")
+}
+
+fun succeededFuture(result: String) = object: AsyncResult<String> {
+    override fun result(): String = result
+    override fun cause(): Throwable = Throwable()
+    override fun succeeded() = true
+    override fun failed() = false
+}
+
+fun sayHello(name: String) {
+    concatWithHello(name, object: Handler<AsyncResult<String>> {
+        override fun handle(f: AsyncResult<String>) {
+            println(f.result())
+        }
+    })
+}
+
+fun concatWithHello(name: String, handler: Handler<AsyncResult<String>>) {
+    handler.handle(succeededFuture("Hello ${name}"))
+}
