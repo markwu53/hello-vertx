4de7f8c023b124a649359c135fbb4257fc814bd6
Mark Wu <mang.wu@gmail.com>
2017-12-12 22:22:49 -0500

    kotlin lambda

--- hello-vertx/src/main/java/hello/hello2.kt
+++ hello-vertx/src/main/java/hello/hello2.kt
@@ -8,10 +8,24 @@ fun main(args: Array<String>) {
     sayHello2("Mark")
 }
 
-fun sayHello2(name: String) = concatWithHello2(name) { future ->
-    println(future.result())
+fun sayHello2(name: String) {
+    concatWithHello2(name) { hello ->
+        concatWithExclamation2(hello.result()) { exclamation ->
+            concatWithISay2(exclamation.result()) { future ->
+                println(future.result())
+            }
+        }
+    }
 }
 
 fun concatWithHello2(name: String, handle: (AsyncResult<String>) -> Unit) {
     handle(succeededFuture("Hello ${name}"))
+}
+
+fun concatWithExclamation2(hello: String, handle: (AsyncResult<String>) -> Unit) {
+    handle(succeededFuture("${hello}!"))
+}
+
+fun concatWithISay2(message: String, handle: (AsyncResult<String>) -> Unit) {
+    handle(succeededFuture("""I say: "${message}""""))
 }
