3dbbb859b45ee589f3e790d0da7179c8a42570cb
Mark Wu <mang.wu@gmail.com>
2017-12-14 23:20:32 -0500

    subscribeOn

--- hello-vertx/src/main/java/hello/HelloRx.java
+++ hello-vertx/src/main/java/hello/HelloRx.java
@@ -7,6 +7,30 @@ import io.reactivex.schedulers.Schedulers;
 public class HelloRx {
 
     public static void main(String[] args) {
+        test03();
+    }
+  
+    public static void test03() {
+        Flowable.just("Alpha", "Beta", "Gamma")
+        .subscribeOn(Schedulers.computation())
+        .map(String::length).subscribe(v -> {
+            System.out.println("received " + v + " on thread " + Thread.currentThread().getName());
+        });
+        try {
+            System.out.println("sleep on " + Thread.currentThread().getName());
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void test02() {
+        Flowable.just("Alpha", "Beta", "Gamma").map(String::length).subscribe(v -> {
+            System.out.println("received " + v + " on thread " + Thread.currentThread().getName());
+        });
+    }
+
+    public static void test01() {
         Flowable.just("Hello World").subscribe(System.out::println);
         //Flowable.range(1, 10) .observeOn(Schedulers.computation()) .map(v -> v * v) .blockingSubscribe(System.out::println);
         /*
