90b4809b3f9e3c7dab49419ca561165fc7c824f6
Mark Wu <mang.wu@gmail.com>
2017-12-17 00:34:15 -0500

    kotlin lambda test

--- hello-vertx/src/main/java/hello/helloAsync.kt
+++ hello-vertx/src/main/java/hello/helloAsync.kt
@@ -3,23 +3,66 @@ package hello
 import io.reactivex.Observable
 import io.reactivex.schedulers.Schedulers
 
+val helloFuture2 = { name: String ->
+    Observable.just(name)
+            .subscribeOn(Schedulers.computation())
+            .map({ println("before sleep"); Thread.sleep(3000); "Hello ${it}"; })
+            .toFuture()
+}
+
+fun mt(bin: (Int, Int) -> Int): Unit {
+    println(bin(3, 6))
+}
+
+fun nt(uni: (Int) -> Int): Unit {
+    println(uni(5))
+}
+
+fun five(uni: (Int) -> Unit) = uni(5)
+fun ma(uni: (Int) -> Unit) = uni(5)
+
 fun helloFuture(name: String) = Observable.just(name)
         .subscribeOn(Schedulers.computation())
-        .map a@ { name ->
+        .map {
             println("before sleep")
             Thread.sleep(3000)
-            return@a "Hello ${name}"
+            "Hello ${it}"
         }
         .toFuture()
 
-fun test02() {
-    Observable.just(helloFuture("Mark"))
+val test01 = {
+    Observable.just(helloFuture2("Mark"))
             .subscribeOn(Schedulers.computation())
-            .map { f -> f.get() }.subscribe { println(it) }
+            .map { it.get() }.subscribe { println(it) }
     println("before long sleep")
     Thread.sleep(6000)
 }
 
-fun main(args: Array<String>) {
-    test02()
+fun test02() {
+    Observable.just(helloFuture("Mark"))
+            .subscribeOn(Schedulers.computation())
+            .map { it.get() }.subscribe { println(it) }
+    println("before long sleep")
+    Thread.sleep(6000)
 }
+
+fun test03() = println("hello")
+
+val test = { a: String -> "append $a" }("hello")
+val test2 = { "message" }()
+
+fun main(args: Array<String>) {
+    //test01()
+    //test02()
+    //println(test2)
+    //mt({ a, b -> a * b })
+    //mt { a, b -> a * b }
+    //nt { it * it }
+    //nt { it.times(1.5).toInt() }
+    five { println(it * it) }
+}
+
+
+
+
+
